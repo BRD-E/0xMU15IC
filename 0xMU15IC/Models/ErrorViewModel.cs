0bfb0422dfbe91ed436f52b959bb8a0bb5ca8989
Joshua Melton <BRD-E@outlook.com>
2018-02-04 18:46:49 -0700

    I fixed a build error, I had accidently removed ErrorViewModels.cs when cleaning the project for the first commit. Additionally I've noticed I need to set up .gitignore rules for this project.

---
+++ 0xMU15IC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _0xMU15IC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
